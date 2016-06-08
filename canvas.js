aee226a8aeee687d8e9d4fb0fef1035d47c07081
PaulineND <aysha.pauline@gmail.com>
2016-06-08 22:00:17 +0200

    creation de canvas

---
+++ canvas.js
@@ -0,0 +1,49 @@
+var canvas = document.getElementById('myCanvasGauche');
+var context = canvas.getContext('2d');
+
+	context.beginPath();
+	context.moveTo(100, 100);
+	context.lineTo(300, 100);
+	context.lineWidth = 3;
+	context.stroke();
+	// set line color
+	context.strokeStyle = '#99FF99';
+	context.stroke();
+
+
+var canvas = document.getElementById('myCanvasDroite');
+var context = canvas.getContext('2d');
+
+		context.beginPath();
+		context.moveTo(100, 100);
+		context.lineTo(300, 100);
+		context.lineWidth = 3;
+		context.stroke();
+		// set line color
+		context.strokeStyle = '#99FF99';
+		context.stroke();
+
+
+var canvas = document.getElementById('myCanvasHaut');
+var context = canvas.getContext('2d');
+
+		context.beginPath();
+		context.moveTo(200, 100);
+		context.lineTo(200, 300);
+		context.lineWidth = 3;
+
+	     // set line color
+	    context.strokeStyle = '#99FF99';
+	    context.stroke();
+
+var canvas = document.getElementById('myCanvasBas');
+var context = canvas.getContext('2d');
+
+		context.beginPath();
+		context.moveTo(200, 000);
+		context.lineTo(200, 200);
+		context.lineWidth = 3;
+
+	     // set line color
+	    context.strokeStyle = '#99FF99';
+	    context.stroke();
